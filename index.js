221981b839cc4cbab5c00a837969665a6e6ae15d
vansh-makhija <www.vansh1624@gmail.com>
2026-01-12 18:43:57 +0530

    cors issue

--- index.js
+++ index.js
@@ -42,11 +42,11 @@ dotenv.config();
 const app = express();
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
-app.use(cors({
-  origin: process.env.CLIENT_URL ,
-  credentials: true
-}));
-//  app.use(cors())
+// app.use(cors({
+//   origin: process.env.CLIENT_URL ,
+//   credentials: true
+// }));
+ app.use(cors())
 
 
 // app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
